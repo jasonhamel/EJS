4caeadb7b03f591ef7286d7a3f479dfa5447798f
Jason <github@jasongetsemail.com>
2023-07-16 15:21:28 -0400

    Build the the first template using EJS

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
+
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   let today = new Date();
-  if (today.getDay() == 6 || today.getDay() == 0) {
-    res.send("Keep if fun, its the weekend");
-  } else {
-    res.send("Keep it professional, its a weekday");
+  let currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error!! No matching day: " + value);
   }
-  res.send("<h1>Hello, Jason</h1>");
+  res.render("list", { dayOfWeek: day });
 });
 
 app.listen(3000, function () {
