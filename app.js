c41f9c5c1be3f616c33308ead5ef95353ceefe04
Jason <github@jasongetsemail.com>
2023-07-16 19:55:44 -0400

    Impliment static style created by the instructor

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import ejs from "ejs";
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 let items = [];
 
 app.get("/", function (req, res) {
